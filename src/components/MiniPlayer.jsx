cd671830aae1ff230a9a6cf1015a48283d072f76
Inzamul Haque <inzamul119@gmail.com>
2021-12-22 02:26:51 +0600

    add toggle feature to MiniPlayer component

--- src/components/MiniPlayer.jsx
+++ src/components/MiniPlayer.jsx
@@ -1,16 +1,33 @@
+import { useState } from 'react';
 import img from '../images/3.jpg';
 import classes from '../styles/MiniPlayer.module.css';
 
 function MiniPlayer({ title, thumbnail }) {
+  const [float, setFloat] = useState(true);
+
+  const floatBtnClass = `${classes.miniPlayer} ${
+    float ? classes.floatingBtn : ''
+  }`;
+
+  console.log(floatBtnClass);
+  console.log(float);
+
+  const toggleFloat = (e) => {
+    e.stopPropagation();
+    setFloat((prevState) => !prevState);
+  };
+
   return (
-    <div
-      className={`${classes.miniPlayer} ${classes.floatingBtn}`}
-      onClick={() => {}}
-    >
+    <div className={floatBtnClass} onClick={toggleFloat}>
       <span className={`material-icons-outlined ${classes.open}`}>
         play_circle_filled
       </span>
-      <span className={`material-icons-outlined ${classes.close}`}>close</span>
+      <span
+        className={`material-icons-outlined ${classes.close}`}
+        onClick={toggleFloat}
+      >
+        close
+      </span>
       <img src={thumbnail || img} alt='Video thumbnail' />
       <p>{title}</p>
     </div>
