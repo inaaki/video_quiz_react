7f6b0585d54edf3b5dcb03dd058bf53f93e07f3b
Inzamul Haque <inzamul119@gmail.com>
2021-12-22 00:09:26 +0600

    change App component's directory

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Layout from './components/Layout';
+import Login from './components/pages/Login';
+import './styles/App.css';
+// import Home from './pages/Home';
+// import SignUp from './components/pages/SignUp';
+
+function App() {
+  return (
+    <Layout>
+      {/* <Home /> */}
+      {/* <SignUp /> */}
+      <Login />
+    </Layout>
+  );
+}
+
+export default App;
