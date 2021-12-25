404d84190fc3b5a8b395cb929ec6751111508f8b
Inzamul Haque <inzamul119@gmail.com>
2021-12-25 22:22:48 +0600

    prevent logged in user from accessing login and signup page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Quiz from './components/pages/Quiz';
 import Result from './components/pages/Result';
 import SignUp from './components/pages/SignUp';
 import PrivateRoute from './components/routes/PrivateRoute';
+import PublicRoute from './components/routes/PublicRoute';
 import './styles/App.css';
 
 function App() {
@@ -21,8 +22,25 @@ function App() {
             </PrivateRoute>
           }
         />
-        <Route exact path='/login' element={<Login />} />
-        <Route exact path='/signup' element={<SignUp />} />
+
+        <Route
+          exact
+          path='/login'
+          element={
+            <PublicRoute>
+              <Login />
+            </PublicRoute>
+          }
+        />
+        <Route
+          exact
+          path='/signup'
+          element={
+            <PublicRoute>
+              <SignUp />
+            </PublicRoute>
+          }
+        />
         <Route
           exact
           path='/result'
