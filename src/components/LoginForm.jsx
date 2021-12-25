6e730d16cd8619ed3ba2accc34787352a3bcdabe
Inzamul Haque <inzamul119@gmail.com>
2021-12-25 21:24:24 +0600

    add login functinality for loginForm component

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,16 +1,67 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Button from '../Button';
-import Form from '../Form';
-import TextInput from '../TextInput';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
+import Button from './Button';
+import Form from './Form';
+import FormError from './FormError';
+import TextInput from './TextInput';
 
 function LoginForm() {
+  const [err, setErr] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const [form, setForm] = useState({
+    email: '',
+    pass: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    const newForm = { ...form };
+    newForm[name] = value;
+    setForm(newForm);
+  };
+
+  const { login } = useAuth();
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { email, pass } = form;
+    
+    setLoading(true);
+    login(email, pass)
+      .then(() => {
+        navigate('/', { option: true });
+      })
+      .catch(() => {
+        setErr('Unable to log in, please try again');
+        setLoading(false);
+      });
+  };
   return (
-    <Form className='login'>
-      <TextInput icon='alternate_email' placeholder='Enter email' type='text' />
-      <TextInput icon='lock' placeholder='Enter password' type='password' />
-      <Button>Login</Button>
-      <div class='info'>
+    <Form className='login' onSubmit={handleSubmit}>
+      <TextInput
+        icon='alternate_email'
+        placeholder='Enter email'
+        type='text'
+        name='email'
+        value={form.email}
+        onChange={handleChange}
+      />
+      <TextInput
+        name='pass'
+        value={form.pass}
+        icon='lock'
+        placeholder='Enter password'
+        type='password'
+        onChange={handleChange}
+      />
+      <Button type='submit' disabled={loading}>
+        Login
+      </Button>
+
+      {err && <FormError text={err} />}
+      <div className='info'>
         Don't have an account? <Link to='/signup'>Signup</Link> instead.
       </div>
     </Form>
