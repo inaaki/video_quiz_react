27327a7635cc21684cab5b17bc73930234b9c59a
Inzamul Haque <inzamul119@gmail.com>
2021-12-25 21:52:30 +0600

    add logout functionality to Account component

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,15 +1,47 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
 import classes from '../styles/Account.module.css';
 
 function Account() {
+  const { currentUser, logout } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout()
+      .then(() => {
+        console.log('im in then');
+        navigate('/login');
+      })
+      .catch((e) => {
+        alert('Failed to Log out');
+      });
+  };
+
   return (
     <div className={classes.account}>
       <span className='material-icons-outlined' title='Account'>
         account_circle
       </span>
-      <Link to='/signup'>Signup</Link>
-      <Link to='/login'>Login</Link>
-      {/* <span className="material-icons-outlined" title="Logout"> logout </span> */}
+
+      {!currentUser ? (
+        <>
+          <Link to='/signup'>Signup</Link>
+          <Link to='/login'>Login</Link>
+        </>
+      ) : (
+        <>
+          {currentUser?.email}{' '}
+          <span
+            className='material-icons-outlined'
+            title='Logout'
+            onClick={handleLogout}
+            style={{ marginLeft: '1em' }}
+          >
+            {' '}
+            logout{' '}
+          </span>
+        </>
+      )}
     </div>
   );
 }
