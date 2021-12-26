90d1a4cb65ddfced3bc52062856fefcf9e21381f
Inzamul Haque <inzamul119@gmail.com>
2021-12-26 23:08:15 +0600

    add database url

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,13 @@
 import { initializeApp } from 'firebase/app';
-import {
-  createUserWithEmailAndPassword,
-  getAuth,
-  signInWithEmailAndPassword,
-  signOut
-} from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
+  appId: process.env.REACT_APP_APP_ID,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DB_URL,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
