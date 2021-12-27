25a62bc588b79cc90a699e0fd47a33cb0dd3b297
Inzamul Haque <inzamul119@gmail.com>
2021-12-27 14:36:49 +0600

    'add feature to response error'

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,7 +1,15 @@
+import useVideoList from '../../hooks/useVideoList';
 import Videos from '../Videos';
 
 function Home() {
-  return <Videos />;
+  const { videos, err } = useVideoList();
+  const length = videos.length;
+
+  return length ? (
+    <Videos {...videos} />
+  ) : (
+    <p style={{ textAlign: 'center' }}>{err}</p>
+  );
 }
 
 export default Home;
