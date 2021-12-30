103588fb78d349e07d269439d5d9f0be8e4c207b
Inzamul Haque <inzamul119@gmail.com>
2021-12-31 01:32:30 +0600

    update:  create dynamic analysis component

--- src/components/Analysis.jsx
+++ src/components/Analysis.jsx
@@ -1,17 +1,15 @@
 import style from '../styles/Analysis.module.css';
 import Question from './Question';
 
-function Analysis() {
+function Analysis({ questions }) {
+  console.log('inside analysis', questions);
   return (
     <div className={style.analysis}>
       <h1>Question Analysis</h1>
-      <h4>You answerd 5 out of 10 questions correctly</h4>
-
-      <Question />
-      <Question />
-      <Question />
-      <Question />
-      <Question />
+      {/* <h4>You answerd 5 out of 10 questions correctly</h4> */}
+      {questions.map((question, index) => (
+        <Question key={index} question={question} />
+      ))}
     </div>
   );
 }
