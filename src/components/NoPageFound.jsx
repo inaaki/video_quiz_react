9bd4a649517e7b92d3f1fe2b1b825ef8cd96cc39
Inzamul Haque <inzamul119@gmail.com>
2022-01-03 13:35:19 +0600

    create 404 page

---
+++ src/components/NoPageFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function NoPageFound() {
+  return (
+    <div style={{ display: 'grid', placeContent: 'center' }}>
+      {' '}
+      404 No Page Found
+    </div>
+  );
+}
+
+export default NoPageFound;
